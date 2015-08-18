53be3230a8038c5213180c92c03aa21a8b2dbdcb
Bill <BobcatBill@users.noreply.github.com>
2015-08-18 14:36:19 -0500

    Update temperature.sh

--- temperature.sh
+++ temperature.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-/usr/lib/hobbit/client/bin/bb xymon "status `hostname`.temperature green `date`
+XYMONSRV="XYMON SERVER"
+/usr/lib/hobbit/client/bin/bb $XYMONSRV "status `hostname`.temperature green `date`
 `awk '{printf("&green CPU %.0f %.0f\n",$1/1e3,(($1*1.8)/1e3)+32)}' /sys/class/thermal/thermal_zone0/temp`
 "
