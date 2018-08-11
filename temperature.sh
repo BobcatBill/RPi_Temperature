c56a60f6ba71eecc234cc164111fbec06d534ebc
Bill <BobcatBill@users.noreply.github.com>
2018-08-11 16:22:36 -0500

    Update temperature.sh

--- temperature.sh
+++ temperature.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-XYMONSRV="XYMON SERVER"
-
 $XYMON $XYMONSRV "status `hostname`.temperature green `date`
 `awk '{printf("&green CPU %.0f %.0f\n",$1/1e3,(($1*1.8)/1e3)+32)}' /sys/class/thermal/thermal_zone0/temp`
 "
